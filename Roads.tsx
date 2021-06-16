dfc4af5a01a626ff775d35ca5e82ebebc2f435ea
julia-harbord <61981390+julia-harbord@users.noreply.github.com>
2021-06-16 16:52:33 -0400

    Add files via upload

---
+++ Roads.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Roads" tilewidth="16" tileheight="16" tilecount="256" columns="32">
+ <image source="../Downloads/Modern City/Street.png" width="512" height="128"/>
+</tileset>
