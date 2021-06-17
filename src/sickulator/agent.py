77720662d1d15931a3072c046fca567b7f546714
Jonah Kornberg <kornberg.jonah@gmail.com>
2021-06-17 16:25:12 -0400

    Add family class

--- src/sickulator/agent.py
+++ src/sickulator/agent.py
@@ -8,17 +8,50 @@ class HealthState(Enum):
     IMMUNE = 2
     DEAD = 3
 
-
 class Agent(pg.sprite.Sprite):
-    def __init__(self,game,x,y,health_state=HealthState.HEALTHY):
-        self.pos = vec(x,y)
+    def __init__(self, game, family, x, y, health_state=HealthState.HEALTHY):
+        self._pos = vec(x,y)
         self.game = game
         self.groups = game.all_sprites
+        self.family = family
         pg.sprite.Sprite.__init__(self, self.groups)
-        self.birth_tick = game.clock.get_ticks()
-        self.health_state = health_state
+        self._birth_tick = game.clock.get_ticks()
+        self._health_state = health_state
 
-
-
+    @property
+    def health_state(self):
+        return self._health_state
     
+    @health_state.setter
+    def health_state(self, hs : HealthState):
+        self._health_state = hs
+
+    @property
+    def pos(self):
+        return self._pos
+    
+    @pos.setter
+    def pos(self, x, y):
+        self._pos = vec(x,y)
+
+
+class Family():
+    """
+    Represents a family unit of agents.
+    
+    Arguments:
+    
+    game -- Game object which contains all world info (clock etc)
+    """
+    count = 0
+
+    def __init__(self, game):
+        self.game = game
+        self.agents = []
+        self.work = 0
+        Family.count += 1
+    
+    def add_agent(self, agent : Agent):
+        """Add agent to a family's agents list"""
+        self.agents.append(agent)
 
