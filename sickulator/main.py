347f276112b1a0def0e91b1631c135ca52c943f6
Dahlia La P <59484420+DahliaL@users.noreply.github.com>
2021-06-20 17:51:51 -0400

    fixed formatting

--- sickulator/main.py
+++ sickulator/main.py
@@ -1,18 +1,9 @@
-# KidsCanCode - Game Development with Pygame video series
-# Tile-based game - Part 2
-# Collisions and Tilemaps
-# Video link: https://youtu.be/ajR4BZBKTr4
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import os
 from game import Game
 os.environ["SDL_VIDEODRIVER"]="x11"
 os.environ['SDL_AUDIODRIVER'] = 'dsp'
-
-
-
-
-
 
 
 if __name__ == "__main__":
