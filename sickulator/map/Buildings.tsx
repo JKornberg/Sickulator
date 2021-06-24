b598c437beb467917cb0022f87f02cf7e5d03bf8
DahliaL <dahlia.lapom@gmail.com>
2021-06-24 15:41:31 -0400

    clean up

--- sickulator/map/Buildings.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Buildings" tilewidth="16" tileheight="16" tilecount="180" columns="12">
- <image source="Buildings.png" width="192" height="240"/>
-</tileset>
--- sickulator/map/Roads.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Roads" tilewidth="16" tileheight="16" tilecount="256" columns="32">
- <image source="Street.png" width="512" height="128"/>
-</tileset>
